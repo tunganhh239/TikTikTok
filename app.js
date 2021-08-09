16720cdbccf7355de1e7c93ed88cafca0e2354cb
Tung Anh <52038792+tunganhh239@users.noreply.github.com>
2021-08-09 10:09:17 +0700

    Sua bien

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 javascript:
 var host = window.location.hostname;
 var pathname = window.location.pathname;
-var https_host = "https://" + host + pathname;
+var https_url = "https://" + host + pathname;
 var x = document.getElementsByClassName("video-card-browse")[0].getElementsByTagName('video')[0].src;
 window.open(x, '_blank');   
